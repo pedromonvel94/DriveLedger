89307a6a440f8014a7cea87dcd013db75dec3d63
Pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2026-01-23 23:33:26 -0500

    I created the logic where I first create a URL object to store the URL. I do this to get the userId parameter (the userId number) from the URL. Then I check that it isn’t null; if it is, I return an error with status 400. Next, I parse the value into an integer. If the string didn’t contain a number, it returns NaN, so if it’s NaN I return another 400 error. Finally, if everything goes well and I get a valid number, I simply return the id with a 200 ok status

--- src/app/api/vehicles/route.ts
+++ src/app/api/vehicles/route.ts
@@ -2,10 +2,25 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(request: Request) {
     try {
-        var url = new URL(request.url);
-        var userId = url.searchParams.get("userId");
-        return Response.json({ user_id: userId });
+        const url = new URL(request.url);
+        const userIdParam = url.searchParams.get("userId");
+
+        if (userIdParam === null) {
+            return Response.json({ error: "userId is required" }, { status: 400 });
+        }
+
+        const userId = parseInt(userIdParam, 10);
+
+        if (isNaN(userId)) {
+            return Response.json({ error: "userId must be a number" }, { status: 400 });
+        } else{
+            return Response.json({ ok: true, data: { userId: userIdParam}}, { status: 200 });
+        }
+
+        
     } catch (e) {
+
+        //Me falta implementar bien el manejo de errores OJO!!!
         return Response.json({ ok: false, error: { code: "INTERNAL_ERROR", message: "Database error" } }, { status: 500 });
     }
 }
