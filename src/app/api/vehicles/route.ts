5de9c462fe3de8dae13bf819de11feb1098d9879
Pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2026-01-29 20:27:10 -0500

    Refactoring the code, leaving the POST method ready to save the data with Prisma and return the created vehicle

--- src/app/api/vehicles/route.ts
+++ src/app/api/vehicles/route.ts
@@ -44,9 +44,12 @@ export async function POST(request: Request) {//Recibimos el request que basicam
         { status: 200 }
         );
         */
+        const status400Request = (message: string) => Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message } }, { status: 400 });
+        
+
 
         var body = await request.json(); //Obtengo el cuerpo de la peticion HTTP en formato JSON
-          
+
         /*
         AQUI COLOCO VARIOS LOGUEOS PARA DEBUQUEAR EL CONTENIDO DEL BODY QUE LLEGA DESDE POSTMAN U OTRO CLIENTE HTTP
         console.log("Body received in POST /api/vehicles:", body); //Logueo el body recibido para debuggear
@@ -54,48 +57,76 @@ export async function POST(request: Request) {//Recibimos el request que basicam
         console.log("Type of user_id:", typeof body.user_id); //Logueo el tipo de dato del user_id para debuggear
         console.log("ownership_type in body:", body.ownership_type); //Logueo el ownership_type recibido para debuggear
         */
-
-        if(body.user_id === undefined || body.ownership_type === undefined) {
-            return Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message: "userId and ownershipType are required"} }, { status: 400 });
-        };
         
         const userId = parseInt(body.user_id, 10); //Convierto el user_id de string a numero entero base 10 (decimal)
         const ownership_type = body.ownership_type; 
-        const weeklyRentAmount = parseInt(body.weekly_rent_amount, 10);
+        var weeklyRentAmount = null;
+        var alias = null; 
+        var plate = null; 
 
-        /*
-        MAS LOGUEOS PARA DEBUQUEAR EL CONTENIDO DEL BODY QUE LLEGA DESDE POSTMAN U OTRO CLIENTE HTTP
-        console.log("Parsed userId:", userId); //Logueo el userId parseado para debuggear
-        console.log("Weekly rent amount in body:", weeklyRentAmount); //Logueo el weeklyRentAmount recibido para debuggear
-        */
+        console.log("keys:", Object.keys(body))
+        console.log("alias:", body.alias, "type:", typeof body.alias)
+        console.log("plate:", body.plate, "type:", typeof body.plate)
 
-        if(isNaN(userId) || isNaN(weeklyRentAmount)) {//Valido que el userId o weeklyRentAmount sean un numero si cuaquiera de los 2 no lo es devuelvo un error de validacion porque me devuelve NaN
-
-            return Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message: "userId or weeklyRentAmount must be numbers" } }, { status: 400 });
-
-        } else if(ownership_type !== "OWNED" && ownership_type !== "RENTED"){
-
-            return Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message: "ownershipType must be either OWNED or RENTED" } }, { status: 400 });
-
-        } else{
-            if(ownership_type === "RENTED" && weeklyRentAmount > 0){
-
-                return Response.json({ ok: true, data: body}, { status: 201 });
-
-            } else if(ownership_type === "RENTED" && (weeklyRentAmount === undefined || weeklyRentAmount <= 0)){
-
-                return Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message: "weeklyRentAmount is required and must be greater than 0 when ownershipType is RENTED"} }, { status: 400 });
-
-            } else if (ownership_type === "OWNED" && (weeklyRentAmount === undefined || weeklyRentAmount === 0)){
-
-                return Response.json({ ok: true, data: body}, { status: 201 });
-
-            } else {
-
-                return Response.json({ "ok": false, error: {code:"VALIDATION_ERROR", message: "weeklyRentAmount must be 0 or undefined when ownershipType is OWNED"} }, { status: 400 });
-            }
-            
+        if(body.user_id === undefined || body.ownership_type === undefined) {
+            return status400Request("userId and ownershipType are required");
         };
+
+        if(isNaN(userId)) {//Valido que el userId sea un numero si no lo es devuelvo un error de validacion porque me devuelve NaN
+            return status400Request("userId must be a number");
+        };
+
+        if(ownership_type !== "OWNED" && ownership_type !== "RENTED"){ //Valido que el ownership_type sea OWNED o RENTED
+            return status400Request("ownershipType must be either OWNED or RENTED");
+        }
+        
+        if(ownership_type === "RENTED") { // Si el ownership_type es RENTED, entonces el weekly_rent_amount es obligatorio y debe ser mayor a 0
+            weeklyRentAmount = parseInt(body.weekly_rent_amount, 10)
+            if(isNaN(weeklyRentAmount) || weeklyRentAmount <= 0){
+                return status400Request("weeklyRentAmount is required and must be greater than 0 when ownershipType is RENTED");
+            }
+        }
+
+        if(body.alias != null) {
+            if(typeof body.alias !== "string") {//Valido que el alias sea un string
+                return status400Request("alias must be a string with at least 2 characters");
+            }
+
+            alias = body.alias.trim();//Elimino espacios en blanco al inicio y al final del alias
+
+            if(alias.length < 2) { //Valido que el alias sea un string con al menos 2 caracteres
+                return status400Request("alias must be a string with at least 2 characters");
+            }
+        }
+
+        console.log("alias after processing:", alias);
+        
+        if(body.plate != null) {
+            if(typeof body.plate !== "string") {//Valido que la patente sea un string
+                return status400Request("plate must be a string with at least 6 characters");
+            }
+
+            plate = body.plate.replaceAll(' ', '').toUpperCase(); //Elimino espacios en blanco y convierto a mayusculas la patente
+            
+            if(plate.length < 6) { //Valido que la patente sea un string con al menos 6 caracteres
+                return status400Request("plate must be a string with at least 6 characters");
+            }
+        }
+
+        console.log("plate after processing:", plate);
+        
+        const payload = {
+            user_id: userId,
+            ownership_type: ownership_type,
+            weekly_rent_amount: ownership_type === "OWNED" ? null : weeklyRentAmount,
+            alias: alias,
+            plate: plate
+        }
+
+        const statusOk201Request = (data:any) => Response.json({ ok: true, data }, { status: 201 });
+        
+        return statusOk201Request(payload); //Temporalmente devuelvo el body recibido para debuggear antes de hacer la insercion en la base de datos
+
     } catch (e) {
         console.log(e);
         return Response.json({ ok: false, error: { code: "INTERNAL_ERROR", message: "Database error" } }, { status: 500 });
